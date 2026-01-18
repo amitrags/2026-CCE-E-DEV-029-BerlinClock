18d31e26dba5ded66ba98d30bdd58bbcef0d2795
Amit Kumar <AmitRags@gmail.com>
2026-01-18 23:06:12 +0100

    HoursTest added: Green

--- src/main/java/com/example/berlinclock/service/impl/HoursLampServiceImpl.java
+++ src/main/java/com/example/berlinclock/service/impl/HoursLampServiceImpl.java
@@ -3,16 +3,32 @@ package com.example.berlinclock.service.impl;
 import com.example.berlinclock.dto.LampColor;
 import com.example.berlinclock.service.HoursLampService;
 
- class HoursLampServiceImpl implements HoursLampService {
+import java.util.Arrays;
+import static com.example.berlinclock.dto.LampColor.*;
+
+class HoursLampServiceImpl implements HoursLampService {
+
+  private static final int LAMP_COUNT = 4;
+
   //The upper row represents 5 hour blocks and is made up of 4 red lamps.
   @Override
   public LampColor[] getFiveHoursLamps(int hours) {
-    return new LampColor[4];
+    LampColor[] lamps = new LampColor[LAMP_COUNT];
+    Arrays.fill(lamps, O);
+
+    int litCount = hours / 5;
+    Arrays.fill(lamps, 0, litCount, R);
+    return lamps;
   }
-  
+
   //The lower row represents 1 hour blocks and is also made up of 4 red lamps.
   @Override
   public LampColor[] getOneHoursLamps(int hours) {
-    return new LampColor[4];
+    LampColor[] lamps = new LampColor[LAMP_COUNT];
+    Arrays.fill(lamps, O);
+
+    int litCount = hours % 5;
+    Arrays.fill(lamps, 0, litCount, R);
+    return lamps;
   }
 }
